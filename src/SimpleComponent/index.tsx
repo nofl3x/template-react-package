e49ada82b5cf46ffdc86946961d53ced554344e8
nofl3x <nofl3x@protonmail.com>
2020-01-28 21:45:51 +0300

    Add simple component

---
+++ src/SimpleComponent/index.tsx
@@ -0,0 +1,25 @@
+// This file contains a simple react component
+import * as React from 'react'
+
+/**
+ * Component's props
+ */
+export interface ISimpleComponentProps {
+  text: string;
+}
+
+/**
+ * Simple component as an example
+ * @param props
+ * @constructor
+ */
+export function SimpleComponent(props: React.PropsWithChildren<ISimpleComponentProps>) {
+  const { text, children } = props
+
+  return (
+    <div className="simpleComponent">
+      <div className="simpleComponent__text">{text}</div>
+      <div className="simpleComponent__content">{children}</div>
+    </div>
+  )
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { SimpleComponent } from './SimpleComponent'
