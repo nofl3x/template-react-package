65df1bdc58dc3f395f9fe3ad9a71e6977b6d0cd8
nofl3x <nofl3x@protonmail.com>
2020-04-09 17:52:12 +0300

    Fix jest config
    Fix jest config and remove enzyme adapter

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,5 @@ module.exports = {
   },
   "testRegex": "/__tests__/.*\\.(ts|tsx|js)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testEnvironment: "node",
-
-  "setupFiles": [
-    "<rootDir>/src/setupTests.ts"
-  ],
-  "snapshotSerializers": [
-    "enzyme-to-json/serializer"
-  ]
+  testEnvironment: "jsdom"
 }
